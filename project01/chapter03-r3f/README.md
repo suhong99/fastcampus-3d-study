9c76dd825b3968b4a41a2dfbdca5cc67ddf08c23
SUHONG\topic <topic828@Naver.com>
2024-04-08 19:00:15 +0900

    r3f geo, material 실습

---
+++ project01/chapter03-r3f/README.md
@@ -0,0 +1,3 @@
+# React + Vite
+
+R3F는 three.js와 달리 react 친화적이고, 사용이 간단함
---
+++ project01/chapter03-r3f/src/App.jsx
@@ -0,0 +1,11 @@
+import { MainCanvas } from './components/MainCamvas';
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <MainCanvas />
+    </div>
+  );
+}
+
+export default App;
