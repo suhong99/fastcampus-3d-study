a4cfdb151243aa87e5d05c880301b39bd30bd1ae
SUHONG\topic <topic828@Naver.com>
2024-04-11 22:35:35 +0900

    suspense로 로딩처리

--- project02/example01/src/pages/index.js
+++ project02/example01/src/pages/index.js
@@ -1,6 +1,8 @@
 import { Canvas } from '@react-three/fiber';
 import Lights from '../components/Lights';
 import Scene from '../components/Scene';
+import { Suspense } from 'react';
+import { Loader } from '@react-three/drei';
 
 export function Home() {
   return (
@@ -8,11 +10,12 @@ export function Home() {
       {/* // r3f에서 자동을 카메라 렌더러 scene을 Canvas에서 선언해줌 */}
       <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
         <color attach="background" args={['rgb(67,127,240) 100%)']}></color>
-        {/* <Box rotation-y={1} />
-    <Box position={[0, 0, -1]} rotation-y={1} /> */}
-        <Lights />
-        <Scene />
+        <Suspense fallback={null}>
+          <Lights />
+          <Scene />
+        </Suspense>
       </Canvas>
+      <Loader />
     </>
   );
 }
