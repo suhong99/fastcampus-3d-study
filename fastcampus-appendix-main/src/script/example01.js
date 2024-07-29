8f248926f9e899df67235ad881e85aa6b8d87a43
SUHONG\topic <topic828@Naver.com>
2024-07-29 18:30:21 +0900

    비디오 파일을 스크롤 높이를 통해 재생 조절

--- fastcampus-appendix-main/src/script/example01.js
+++ fastcampus-appendix-main/src/script/example01.js
@@ -1,7 +1,16 @@
-function pageExample01 () {
-  
+function pageExample01() {
+  const video = document.getElementById('video');
+
+  window.addEventListener('scroll', () => {
+    const time = (window.scrollY / 1000) * 4;
+    video.currentTime = time;
+  });
+  window.addEventListener('load', () => {
+    video.pause();
+    video.currentTime = 0;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  pageExample01()
-})
+  pageExample01();
+});
