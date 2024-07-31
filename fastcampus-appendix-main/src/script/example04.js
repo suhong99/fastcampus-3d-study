b3644861b51db14ea92f011b5575c982c96d5088
SUHONG\topic <topic828@Naver.com>
2024-07-31 16:30:53 +0900

    gsap 플러그인을 활용해 스크롤이벤트 추가

--- fastcampus-appendix-main/src/script/example04.js
+++ fastcampus-appendix-main/src/script/example04.js
@@ -1,42 +1,46 @@
-function pageExample04 () {
-  makeMainVideo()
-  makeSection()
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+function pageExample04() {
+  makeMainVideo();
+  makeSection();
 }
 
 const makeMainVideo = () => {
   const html = document.documentElement;
-  const canvas = document.getElementById("scrollAnimation");
-  const context = canvas.getContext("2d");
+  const canvas = document.getElementById('scrollAnimation');
+  const context = canvas.getContext('2d');
   const frameCount = 130;
-  const currentFrame = index => (
-    `assets/img/dog_${index.toString().padStart(3, '0')}.jpg`
-  )
+  const currentFrame = (index) =>
+    `assets/img/dog_${index.toString().padStart(3, '0')}.jpg`;
 
-  const img = new Image()
+  const img = new Image();
   img.src = currentFrame(1);
 
   canvas.width = 1280;
   canvas.height = 740;
-  img.onload = () =>{
+  img.onload = () => {
     context.drawImage(img, 0, 0);
-  }
+  };
 
-  const updateImage = index => {
+  const updateImage = (index) => {
     img.src = currentFrame(index);
     context.drawImage(img, 0, 0);
-  }
+  };
 
-  window.addEventListener('scroll', () => {  
+  window.addEventListener('scroll', () => {
     const scrollTop = html.scrollTop;
-    const maxScrollTop = html.scrollHeight - window.innerHeight
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
     const scrollFraction = scrollTop / maxScrollTop;
-    
+
     const frameIndex = Math.min(
-      frameCount -1,
+      frameCount - 1,
       Math.ceil(scrollFraction * frameCount)
     );
-    
-    requestAnimationFrame(() => updateImage(frameIndex + 1))
+
+    requestAnimationFrame(() => updateImage(frameIndex + 1));
   });
 
   const preloadImages = () => {
@@ -46,13 +50,31 @@ const makeMainVideo = () => {
     }
   };
 
-  preloadImages()
-}
+  preloadImages();
+};
 
 const makeSection = () => {
-  
-}
+  const sections = document.querySelectorAll('.section');
+
+  sections.forEach((section) => {
+    const h1 = section.querySelector('.title');
+
+    gsap.set(h1, { opacity: 0 });
+
+    gsap.to(h1, {
+      opacity: 1,
+      scrollTrigger: {
+        trigger: section,
+        start: 'top center',
+        end: 'bottom center',
+        markers: true,
+        toggleActions: 'play reverse play reverse',
+      },
+      duration: 3,
+    });
+  });
+};
 
 document.addEventListener('DOMContentLoaded', () => {
-  pageExample04()
-})
+  pageExample04();
+});
